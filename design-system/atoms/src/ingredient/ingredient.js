8f08dbe5e28066c7f8d3182b15bf4e45b0fa89af
Isaia Maccali <isaiamaccali@fattureincloud.it>
2024-11-23 20:23:50 +0100

    fine sviluppo progetto finale

--- design-system/atoms/src/ingredient/ingredient.js
+++ design-system/atoms/src/ingredient/ingredient.js
@@ -3,8 +3,8 @@ import { GenericComponent } from "../../../_genericComponent.js";
 class Ingredient extends GenericComponent {
   templateFn = () => {
     const name = this.getAttribute("name");
-    const quantity = this.getAttribute("quantity");
-    return `${name} - ${quantity}`;
+    const quantity = this.getAttribute("quantity") || null;
+    return [name, quantity].filter(Boolean).join(" - ");
   };
   stylesheet = "/design-system/atoms/src/ingredient/ingredient.css";
 
